66e472fca6917e0002ee51ffe27fcae93e07b514
Narendra9640 <80750566+Narendra9640@users.noreply.github.com>
2021-10-20 12:15:07 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Narendra Babu" );
+        System.out.println( "Hello World! Narendra Babu Jaya" );
     }
 }
